19969e148b9acdde0d44047502990e896a8e6cf5
Nash504 <nashdfern@gmail.com>
2025-03-26 11:59:03 +0530

    home css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,28 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <header>
-        <h1>MedAid+</h1>
-        <p>Your instant medical assistance app</p>
-      </header>
+      <div className="home-content">
+        <header>
+          <h1>
+            MedAid<span style={{ color: "blue" }}>+</span>
+          </h1>
+          <i>
+            <p>Your instant medical assistance app</p>
+          </i>
+        </header>
 
-      <nav className="nav-links">
-        <Link to="/first-aid" className="nav-button">First Aid</Link>
-        <Link to="/sos" className="nav-button">SOS Emergency</Link>
-        <Link to="/login" className="nav-button">Login</Link>
-      </nav>
+        <nav className="nav-links">
+          <Link to="/login" className="nav-button">
+            Login
+          </Link>
+          <Link to="/first-aid" className="nav-button secondary">
+            First Aid
+          </Link>
+          <Link to="/sos" className="nav-button secondary">
+            SOS Emergency
+          </Link>
+        </nav>
+      </div>
     </div>
   );
 }
